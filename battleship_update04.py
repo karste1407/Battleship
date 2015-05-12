925ade711f74c61f881066b07242976c408ddf23
karste1407 <karste1407@gmail.com>
2015-05-12 13:08:52 +0200

    Update battleship_update04.py

--- battleship_update04.py
+++ battleship_update04.py
@@ -14,6 +14,7 @@ def print_board(board):
 #///////////////////////////Getting input//////////////////////////////////////////
 def user_row():
 	get_row = raw_input("Enter ship row between 1 and 5")
+	#Not shure if this is the best way of checking that the input is an int
 	if int(get_row) == False:
 		print "You must enter an integer between 1 and 5"
 		get_row = raw_input("Enter ship row...")
@@ -39,6 +40,7 @@ print "Player 1 your up!"
 print "Player 2 look away!"
 print "Place your ship..."
 
+#Not shure if this will call the two functions chronologic and store them as index 0 and 1 in my array. That is what I want it to do
 user1_ship = [user_row(), user_col()]
 
 print_board(board)
@@ -50,6 +52,8 @@ user_2 = [user_row(), user_col()]
 
 
 #///////////////////////guesswork?//////////////////////////////////////////////////
+#Maybe while loops inside while loops is not the best way of running the code over and over until someone sinks the other persons ship
+#What Im expecting is the first inside while loop to break the outer loop if the player hits the other players ship otherwise break itself. Likewise with the second inner loop.
 while True:
 	while True:
 		print "Player 1 your turn"
